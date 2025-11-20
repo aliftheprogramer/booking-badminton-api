d6f1d50ca4c52da57531008d49b34b496bf49f1a
Yogaone <yogawanadityapratama@gmail.com>
2025-11-20 21:28:27 +0700

    feat: update environment variables and Cloudinary configuration

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,13 +1,12 @@
-const cloudinary = require('cloudinary').v2;
-const dotenv = require('dotenv');
+const cloudinary = require("cloudinary").v2;
+const dotenv = require("dotenv");
 
 dotenv.config();
 
-// Konfigurasi Cloudinary dengan kredensial dari file .env
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
